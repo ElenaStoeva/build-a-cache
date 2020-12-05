ec17355a50ca48c90fdf5dfd3b2a8bccc1c0d52d
Pinxuan Huang <pinxhuang@gmail.com>
2020-12-05 13:53:03 -0500

    Work on make_cache

--- p5-cache/cache.c
+++ p5-cache/cache.c
@@ -6,21 +6,22 @@
 #include "cache.h"
 #include "print_helpers.h"
 
-cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol){
+cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol)
+{
   cache_t *cache = malloc(sizeof(cache_t));
 
-  cache->capacity = capacity;      // in Bytes
-  cache->block_size = block_size;  // in Bytes
-  cache->assoc = assoc;            // 1, 2, 3... etc.
+  cache->capacity = capacity;     // in Bytes
+  cache->block_size = block_size; // in Bytes, size of a single block
+  cache->assoc = assoc;           // 1, 2, 3... etc.
 
   // FIX THIS CODE!
   // first, correctly set these 5 variables. THEY ARE ALL WRONG
   // note: you may find math.h's log2 function useful
-  cache->n_total_cache_line = 1;
-  cache->n_set = 1;
-  cache->n_offset_bit = 1;
-  cache->n_index_bit = 1;
-  cache->n_tag_bit = 1;
+  cache->n_total_cache_line = capacity / block_size;
+  cache->n_set = capacity / (assoc * block_size);
+  cache->n_offset_bit = log2(block_size);
+  cache->n_index_bit = log2(cache->n_set);
+  cache->n_tag_bit = 32 - cache->n_index_bit - cache->n_offset_bit;
 
   // next create the cache lines and the array of LRU bits
   // - malloc an array with n_rows
@@ -33,8 +34,10 @@ cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t pro
   // initializes cache tags to 0, dirty bits to false,
   // state to INVALID, and LRU bits to 0
   // FIX THIS CODE!
-  for (int i = 0; i < 1; i++) {
-    for (int j = 0; j < 1; j++) {
+  for (int i = 0; i < 1; i++)
+  {
+    for (int j = 0; j < 1; j++)
+    {
       // body goes here
     }
   }
@@ -42,21 +45,23 @@ cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t pro
   return cache;
 }
 
-unsigned long get_cache_tag(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_tag(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
   return 0;
 }
 
-unsigned long get_cache_index(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_index(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
   return 0;
 }
 
-unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
   return 0;
 }
-
 
 /* this method takes a cache, an address, and an action
  * it proceses the cache access. functionality in no particular order: 
@@ -66,9 +71,9 @@ unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr) {
  * return true if there was a hit, false if there was a miss
  * Use the "get" helper functions above. They make your life easier.
  */
-bool access_cache(cache_t *cache, unsigned long addr, enum action_t action) {
+bool access_cache(cache_t *cache, unsigned long addr, enum action_t action)
+{
   // FIX THIS CODE!
 
-
-  return true;  // cache hit should return true
+  return true; // cache hit should return true
 }
